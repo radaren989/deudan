677ada4a42d3e82509072851e3975af983b6fa67
Tugrul <demirozert@gmail.com>
2023-12-24 16:01:59 +0300

    merge with loginhtml

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { authFunc, registerFunc } = require("../controller/auth");
-
-router.get("", authFunc);
-router.post("", registerFunc);
-
-module.exports = router;
