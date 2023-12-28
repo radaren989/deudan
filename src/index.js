7fdea1fd99e9323e6539a4248229c68beb222f7f
Choncey <74412207+Choncey@users.noreply.github.com>
2023-12-28 16:57:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
 let currentPage = 1;
-const previousBtn = document.getElementById("previousPage");
-const nextBtn = document.getElementById("nextPage");
-const displayAdverts = document.getElementById("displayAdverts");
+const previousBtn = document.getElementById('previousPage');
+const nextBtn = document.getElementById('nextPage');
+const displayAdverts = document.getElementById('displayAdverts');
+const profile = document.getElementById('profile');
 
 displayAdvertList();
 
 function displayAdvertList() {
-    fetch(`/api/advertlist/${currentPage}`)
-        .then((response) => response.json())
-        .then((data) => {
-            for (let i = 0; i < 10; i++) {
-                if (data.data[i]) {
-                    displayAdvert(data.data[i]);
-                }
-            }
-        });
+  fetch(`url/${currentPage}`)
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < 10; i++) {
+        if (data.data[i]) {
+          displayAdvert(data.data[i]);
+        }
+      }
+    });
 }
 
 //displayAdvert(1);
 function displayAdvert(item) {
-    const advertsParent = document.getElementById("displayAdverts");
-    const advertDisplay = document.createElement("article");
-    advertDisplay.innerHTML = `<article id="${item.advert_id}" class="row mb-3">
+  const advertsParent = document.getElementById('displayAdverts');
+  const advertDisplay = document.createElement('article');
+  advertDisplay.innerHTML = `<article id="${item.advert_id}" class="row mb-3">
   <div class="col-8">
       
       <span class="small ml-1"
-          >${item.user_name + " " + item.user_surname}
+          >${item.user_name + ' ' + item.user_surname}
       </span>
       <h6 class="mt-2">
           <strong>
@@ -50,65 +51,91 @@ function displayAdvert(item) {
       />
   </div>
 </article>`;
-    advertsParent.appendChild(advertDisplay);
+  advertsParent.appendChild(advertDisplay);
 }
 
 function formatISODate(isoDateTime) {
-    const date = new Date(isoDateTime);
+  const date = new Date(isoDateTime);
 
-    const options = { year: "numeric", month: "long", day: "numeric" };
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
 
-    const dateTimeFormat = new Intl.DateTimeFormat("tr-TR", options);
+  const dateTimeFormat = new Intl.DateTimeFormat('tr-TR', options);
 
-    const formattedDate = dateTimeFormat.format(date);
+  const formattedDate = dateTimeFormat.format(date);
 
-    return formattedDate;
+  return formattedDate;
 }
 
 function updatePageNumber() {
-    document.getElementById("pageNumber").innerText = currentPage;
-    displayAdvertList();
+  document.getElementById('pageNumber').innerText = currentPage;
+  displayAdvertList();
 }
 
 function nextPage() {
-    currentPage++;
-    updatePageNumber();
+  currentPage++;
+  updatePageNumber();
 }
 
 function previousPage() {
-    if (currentPage > 1) {
-        currentPage--;
-        updatePageNumber();
-    }
+  if (currentPage > 1) {
+    currentPage--;
+    updatePageNumber();
+  }
 }
 
 function goToAdvert(e) {
-    const advertId = e.target.parentElement.parentElement.parentElement.id;
+  const advertId =
+    e.target.parentElement.parentElement.parentElement.parentElement.id;
+  console.log(advertId);
 
-    fetch(`/advert/${advertId}`, {
-        method: "GET",
-    }).then((response) => routePage(response, advertId));
+  fetch(`/advert/${advertId}`, {
+    method: 'GET',
+  }).then((response) => routeAdvertPage(response, advertId));
 }
-const routePage = (response, id) => {
-    switch (response.status) {
-        case 200: //successful
-            window.location.href = `/advert/${id}`;
-            break;
-        case 400: //un successful
-            window.alert("hata");
-            break;
-        case 401:
-            window.alert("hata");
-            break;
-        default:
-            console.error("Unhandled status code:", response.status);
-            break;
-    }
+const routeAdvertPage = (response, id) => {
+  switch (response.status) {
+    case 200: //successful
+      window.location.href = `/advert/${id}`;
+      break;
+    case 400: //un successful
+      window.alert('hata');
+      break;
+    case 401:
+      window.alert('hata');
+      break;
+    default:
+      console.error('Unhandled status code:', response.status);
+      break;
+  }
 };
+
+function goToProfile() {
+  fetch('/profile', {
+    method: 'GET',
+  }).then((response) => routeProfilePage(response));
+}
+const routeProfilePage = (response, id) => {
+  switch (response.status) {
+    case 200: //successful
+      window.location.href = '/profile';
+      break;
+    case 400: //un successful
+      window.alert('hata');
+      break;
+    case 401:
+      window.alert('hata');
+      break;
+    default:
+      console.error('Unhandled status code:', response.status);
+      break;
+  }
+};
+
 function init() {
-    previousBtn.addEventListener("click", previousPage);
-    nextBtn.addEventListener("click", nextPage);
-    displayAdverts.addEventListener("click", goToAdvert);
+  previousBtn.addEventListener('click', previousPage);
+  nextBtn.addEventListener('click', nextPage);
+  displayAdverts.addEventListener('click', goToAdvert);
+  profile.addEventListener('click', goToProfile);
 }
 
 init();
